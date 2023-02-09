9371bef8346ae275de7986c62d1f1d111918bd7c
Mithuna Yoganathan <mithuna.y@gmail.com>
2023-02-09 14:26:39 +1100

    Comparing the speed of the new wave with a wave of the same frequency travelling at speed c

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from matplotlib.animation import FuncAnimation
 # speed of light
 C = 1
 
-
 # plane wave function with a given frequency and optional phase
 def plane_wave(frequency, phase=0):
     w = 2 * np.pi * frequency
@@ -14,25 +13,24 @@ def plane_wave(frequency, phase=0):
         return np.cos(-w * t + w/C * x + phase)
     return plane_wave_w_function
 
-
 # frequency of the light and resonant frequency of the material
 frequency = 0.05
 resonant_frequency = 0.1
 t0 = 0
 
-# # original light unimpeded by medium
-# x = np.linspace(-10, 0, 100)
-# y = plane_wave(frequency)(x, t0)
-# plt.plot(x, y)
-
 t = np.linspace(0, 10, 100)
-n = 50
+n = 100
 
 fig, ax = plt.subplots()
 lines = []
-for i in range(n + 1):
-    line, = ax.plot([], [], color="blue")
-    lines.append(line)
+for i in range(n + 2):
+    if i == 1:
+        line, = ax.plot([], [], color="green")
+        lines.append(line)
+    else:
+        line, = ax.plot([], [], color="blue")
+        lines.append(line)
+
 
 ax.set_xlim(-10, 10)
 ax.set_ylim(-1.2, 1.2)
@@ -43,42 +41,34 @@ def init():
     return lines
 
 def update(frame):
+    # wave in the slices of material
     for i, line in enumerate(lines):
-        x_start = i * 10/n
-        x_end = (i+1) * 10/n
-        x = np.linspace(x_start, x_end, int(100/n))
-        phase = (i + 1) * 0.5 * frequency / (resonant_frequency - frequency) * 10 / n
-        y = plane_wave(frequency, phase)(x, frame)
-        line.set_data(x, y)
+        if i == 0:
+            x_start = -10
+            x_end = 0
+            x = np.linspace(x_start, x_end, 100)
+            phase = 0
+            y = plane_wave(frequency, phase)(x, frame)
+            line.set_data(x, y)
+        elif i == 1:
+            x_start = 0
+            x_end = 10
+            x = np.linspace(x_start, x_end, 100)
+            phase = 2 * np.pi
+            y = plane_wave(frequency * 2.5, phase)(x, frame)
+            line.set_data(x, y)
+        else:
+            x_start = i * 10/n
+            x_end = (i+1) * 10/n
+            x = np.linspace(x_start, x_end, int(100))
+            phase = (i + 1) * 0.5 * frequency / (resonant_frequency - frequency) * 10 / n
+            y = plane_wave(frequency, phase)(x, frame)
+            line.set_data(x, y)
 
-    x_start = -10
-    x_end = 0
-    x = np.linspace(x_start, x_end, 100)
-    phase = 0
-    y = plane_wave(frequency, phase)(x, frame)
-    line.set_data(x, y)
     return lines
-
 
 ani = FuncAnimation(fig, update, frames=100, init_func=init, blit=True)
 plt.show()
-
-
-#
-# # slicing the medium up
-# n = 100
-# step = 10/n
-# for i in range(n):
-#     x_segment = np.linspace(i*step, (i+1)*step, 100)
-#     phase = (i + 1) * 0.5 * frequency / (resonant_frequency - frequency) * 10 / n
-#     y_segment = plane_wave(frequency, phase)(x_segment, t0)
-#     plt.plot(x_segment, y_segment, color="blue")
-# plt.xlabel('x')
-# plt.ylabel('y')
-#
-# plt.title('Light travelling through n layers of electrons properly \n accounting for frequency dependence of shift')
-#
-# plt.show()
 
 
 
